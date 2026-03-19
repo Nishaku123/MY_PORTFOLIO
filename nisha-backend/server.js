407988a09266982ab4ea23b1001d255c64142f94
Nisha Kumari <nishamanjhi630@gmail.com>
2026-03-19 12:31:55 +0530

    fix trust proxy

--- nisha-backend/server.js
+++ nisha-backend/server.js
@@ -13,6 +13,7 @@ dotenv.config();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3001;
 
 app.use(helmet({
